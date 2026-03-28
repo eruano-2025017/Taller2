505e9f573b851d2ce6bca76cc578ffe8ab6fd8a7
eruano-2025017 <estebanguatemala33@gmail.com>
2026-03-27 23:42:07 -0600

    feat: se agrega el repositorio de detalleVenta, DetalleVentaRepository.java

--- src/main/java/com/estebanruano/kinalapp/repository/DetalleVentaRepository.java
+++ src/main/java/com/estebanruano/kinalapp/repository/DetalleVentaRepository.java
@@ -2,7 +2,7 @@ package com.estebanruano.kinalapp.repository;
 
 import com.estebanruano.kinalapp.entity.DetalleVenta;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+//.
 
 public interface DetalleVentaRepository extends JpaRepository<DetalleVenta,Long> {
 }
