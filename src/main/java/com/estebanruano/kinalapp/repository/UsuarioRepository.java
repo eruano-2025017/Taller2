3c3ff4458b230dd6d189c3504c72583349dee045
eruano-2025017 <estebanguatemala33@gmail.com>
2026-04-14 19:51:56 -0600

    fix: actualización UsuarioRepository.java para login

--- src/main/java/com/estebanruano/kinalapp/repository/UsuarioRepository.java
+++ src/main/java/com/estebanruano/kinalapp/repository/UsuarioRepository.java
@@ -5,8 +5,11 @@ import com.estebanruano.kinalapp.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
+
 //.
 //Este es el repositorio de Usuario
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     List<Usuario> findByEstado(int estado);
+    Optional<Usuario> findByUsername(String username);
 }
